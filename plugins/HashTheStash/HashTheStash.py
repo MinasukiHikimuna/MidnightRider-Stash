dfb5369422fde77e02b92a86f646d4a08a2dd02d
Minasuki Hikimuna <minasuki.hikimuna@chiefsclub.com>
2024-07-15 09:25:51 +0300

    HashTheStash: Support SHA-256

--- plugins/HashTheStash/HashTheStash.py
+++ plugins/HashTheStash/HashTheStash.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import hashlib
 import xxhash
 import stashapi.log as logger
 from stashapi.stashapp import StashInterface
@@ -9,6 +10,20 @@ from stashapi.stashapp import StashInterface
 def get_xxhash_of_file(file_path):
     # Create an xxhash object
     hasher = xxhash.xxh64()
+
+    # Open the file in binary mode
+    with open(file_path, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for chunk in iter(lambda: f.read(4096), b""):
+            hasher.update(chunk)
+
+    # Return the hexadecimal representation of the hash
+    return hasher.hexdigest()
+
+
+def get_sha256_of_file(file_path):
+    # Create a SHA-256 hash object
+    hasher = hashlib.sha256()
 
     # Open the file in binary mode
     with open(file_path, "rb") as f:
@@ -57,20 +72,34 @@ if __name__ == "__main__":
 
         for scene in scenes:
             for file in scene["files"]:
+                file_path = file["path"]
+                if not os.path.exists(file_path):
+                    logger.warning(f"File not found: {file_path}")
+                    continue
+
+                fingerprints_to_set = []
+
                 if "fingerprints" not in file or not any(
                     fp["type"] == "xxhash" for fp in file["fingerprints"]
                 ):
-                    file_path = file["path"]
-                    if not os.path.exists(file_path):
-                        logger.warning(f"File not found: {file_path}")
-                        continue
-
                     xxhash_value = get_xxhash_of_file(file_path)
-                    logger.info(f"File: {file_path}, xxhash: {xxhash_value}")
-
-                    stash.file_set_fingerprints(
-                        file["id"], [{"type": "xxhash", "value": xxhash_value}]
+                    fingerprints_to_set.append(
+                        {"type": "xxhash", "value": xxhash_value}
                     )
+
+                if "fingerprints" not in file or not any(
+                    fp["type"] == "sha256" for fp in file["fingerprints"]
+                ):
+                    sha256_value = get_sha256_of_file(file_path)
+                    fingerprints_to_set.append(
+                        {"type": "sha256", "value": sha256_value}
+                    )
+
+                if fingerprints_to_set:
+                    logger.info(
+                        f"File: {file_path}, new fingerprints: {fingerprints_to_set}"
+                    )
+                    stash.file_set_fingerprints(file["id"], fingerprints_to_set)
 
         page += 1
 
