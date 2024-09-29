b463a123351ba520580e5baba43a93800724f969
Minasuki Hikimuna <minasuki.hikimuna@chiefsclub.com>
2024-09-29 10:04:56 +0300

    Querying of tags supported in StashDBClient

--- pandas/libraries/StashDbClient.py
+++ pandas/libraries/StashDbClient.py
@@ -56,6 +56,55 @@ class StashDbClient(object):
         )
         return result
 
+    def query_tags(self):
+        query = """
+            query QueryTags($page: Int!, $per_page: Int!) {
+                queryTags(input: {
+                    page: $page
+                    per_page: $per_page
+                }) {
+                    count
+                    tags {
+                        id
+                        name
+                        description
+                        aliases
+                        deleted
+                        created
+                        updated
+                        category {
+                            id
+                            name
+                            group
+                            description
+                        }
+                    }
+                }
+            }
+        """
+        per_page = 25
+        page = 1
+        all_tags = []
+        total_count = None
+
+        while True:
+            result = self._gql_query(query, {"page": page, "per_page": per_page})
+            if result is None or 'data' not in result or 'queryTags' not in result['data']:
+                break
+
+            tags_data = result['data']['queryTags']
+            all_tags.extend(tags_data['tags'])
+
+            if total_count is None:
+                total_count = tags_data['count']
+
+            if len(all_tags) >= total_count:
+                break
+
+            page += 1
+
+        return all_tags
+
     def _gql_query(self, query, variables=None):
         headers = {"Content-Type": "application/json"}
         if self.api_key:
